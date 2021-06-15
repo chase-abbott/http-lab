50d4a1c2ca2652ae280a87552a9d12bbb7602bb1
Chase <cabbott93@gmail.com>
2021-06-14 18:01:24 -0700

    TDD /, /echo, /red routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,27 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-
+  it('displays plain text hi in the browser', async() => {
+    const res = await request(app).get('/');
+    expect(res.text).toEqual('hi');
+  });
+  it('sends hi through /echo', async() => {
+    const res = await request(app).post('/echo').send('hi');
+    expect(res.text).toEqual('hi');
+  });
+  it('sends an html h1 with the word head', async() => {
+    const res = await request(app).get('/red');
+    expect(res.text).toEqual(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <h1> red </h1>
+    </body>
+    </html>`);
+  });
 });
